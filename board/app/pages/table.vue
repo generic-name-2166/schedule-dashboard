d22f7a0bcf3b71ffb49130d88fd162a58fef65bd
generic-name-2166 <kirbesson277@mail.ru>
2026-03-26 23:34:46 +0000

    feat(client): construction sites table

---
+++ board/app/pages/table.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useScheduleStore } from '~/stores/schedule';
+
+const store = useScheduleStore();
+await store.init();
+</script>
+
+<template>
+  <div>
+    <h1>Стройобъекты</h1>
+  </div>
+  <div class="table">
+    <template v-for="node of store.treelike.nodes" :key="node.id">
+      <p>
+        <button type="button">
+          {{ node.wbsCode }}
+        </button>
+      </p>
+      <p>{{ node.name }}</p>
+      <p>{{ node.start }}</p>
+      <p>{{ node.end }}</p>
+    </template>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.table {
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) minmax(0, 4fr) repeat(2, minmax(0, 1fr));
+}
+</style>
---
+++ board/app/stores/sites.ts
@@ -0,0 +1,11 @@
+import { defineStore } from "pinia";
+
+/* export interface ConstructionSite {
+  ksgId: number;
+}
+
+const MOCK_SITES: ConstructionSite[] = []; */
+
+export const useSitesStore = defineStore("sites-store", () => {
+  return {};
+});
